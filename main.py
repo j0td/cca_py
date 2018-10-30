e68ef03867ba3ffed30f6f4f6c9727947316d7f7
Masato Ebisawa <j0td5ta0d1mt@icloud.com>
2018-10-31 06:38:17 +0900

    complete implement Moore neighborhood

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 import time
 import random
+import itertools
 import colorsys
 
 # Global variables
-_RULE = {'w': int(sys.argv[1]), 'h': int(sys.argv[2]), 'n': int(sys.argv[3]), 'neighbor': sys.argv[4]}
-_COLORS = [list(map(lambda x: int(255 * x), list(colorsys.hsv_to_rgb((i + 1) / _RULE['n'], 1, 1)))) for i in range(_RULE['n'])]
+_RULE = {
+  'w': int(sys.argv[1]), # width
+  'h': int(sys.argv[2]), # height
+  'r': int(sys.argv[3]), # range of neighborhood
+  't': int(sys.argv[4]), # threshold
+  'c': int(sys.argv[5]), # count of states
+  'n': sys.argv[6] # type of neighborhood
+}
+_COLORS = [list(map(lambda x: int(255 * x), list(colorsys.hsv_to_rgb((i + 1) / _RULE['c'], 1, 1)))) for i in range(_RULE['c'])]
 
 def main():
   cells = init()
@@ -13,7 +21,7 @@ def main():
   try:
     while True:
       render(cells)
-      cells = init()
+      cells = next_generation(cells)
       time.sleep(0.2)
   except KeyboardInterrupt:
     when_end()
@@ -33,9 +41,27 @@ def next_generation(cells):
     2d-list next generation cells
   """
 
-  return init()
+  x_expanded_cells = [cells[y][-_RULE['r']:] + cells[y] + cells[y][:_RULE['r']] for y in range(len(cells))]
+  expanded_cells = x_expanded_cells[-_RULE['r']:] + x_expanded_cells + x_expanded_cells[:_RULE['r']]
 
-def is_advance(neighbor_cells):
+  flat_next_generation_cells = []
+  for y in range(_RULE['r'], _RULE['h'] + _RULE['r']):
+    cut_rows = expanded_cells[y - _RULE['r']:y] + expanded_cells[y:y + _RULE['r'] + 1]
+    for x in range(_RULE['r'], _RULE['w'] + _RULE['r']):
+      cut_cells = [r[x - _RULE['r']:x] + r[x:x + _RULE['r'] + 1] for r in cut_rows]
+      flatten_cut_cells = list(itertools.chain.from_iterable(cut_cells))
+      next_state = expanded_cells[y][x] + 1 if is_advance(flatten_cut_cells, expanded_cells[y][x]) else expanded_cells[y][x]
+
+      if next_state < _RULE['c']:
+        flat_next_generation_cells.append(next_state)
+      else:
+        flat_next_generation_cells.append(0)
+
+  next_generation_cells = [list(t) for t in zip(*[iter(flat_next_generation_cells)]*_RULE['w'])]
+
+  return next_generation_cells
+
+def is_advance(neighbor_cells, current_state):
   """
   Return True if neighbor cells contain next generation cells over threshold
   
@@ -50,7 +76,9 @@ def is_advance(neighbor_cells):
     do / don't advance to next generation
   """
 
-  return True
+  advance = neighbor_cells.count(current_state + 1) > _RULE['t'] if current_state < _RULE['c'] - 1 else neighbor_cells.count(0) > _RULE['t']
+
+  return advance
 
 def when_end():
   sys.stdout.write(f'\033[J')
@@ -66,7 +94,7 @@ def init():
   initialized_cells: list
     2d-list cells filled with random state
   """
-  initialized_cells = [[random.randrange(0, _RULE['n']) for i in range(_RULE['w'])] for j in range(_RULE['h'])]
+  initialized_cells = [[random.randrange(0, _RULE['c']) for x in range(_RULE['w'])] for y in range(_RULE['h'])]
   return initialized_cells
 
 def render(cells):
